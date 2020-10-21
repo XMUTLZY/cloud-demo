422adc9f24978d0b68d5dcb2d445b337173cf43f
jake.lin <jake.lin@qeeka.com>
2020-10-21 17:05:35 +0800

    .

---
+++ cloud-service2/src/test/java/jake/cloudservice2/CloudService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jake.cloudservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka-server2/src/test/java/jake/eurekaserver2/EurekaServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jake.eurekaserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
