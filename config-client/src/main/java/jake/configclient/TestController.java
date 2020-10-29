33d832279a471e764acd9bf1a1f571ae28409d4f
jake.lin <jake.lin@qeeka.com>
2020-10-29 13:20:24 +0800

    消息总线bus

--- config-client/src/main/java/jake/configclient/TestController.java
+++ config-client/src/main/java/jake/configclient/TestController.java
@@ -1,6 +1,7 @@
 package jake.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
+@RefreshScope
 public class TestController {
     @Value("${version-number}")
     private String version;
